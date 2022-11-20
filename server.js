30d2df76c7711751b8ffd7efd5217b6abbb02064
cwellis <cwellis2016@gmail.com>
2022-11-20 08:03:22 -0500

    added cors

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const { db } = require('./models/userModel');
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
 
 connectDB();
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
